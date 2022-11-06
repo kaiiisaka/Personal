a791110beb3f41493f581d3a8fc46f771143158e
Alex <panches2001@gmail.com>
2022-11-06 15:00:58 +0300

    footer

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from "./components/UI/navbar/NavBar";
 import {BrowserRouter} from "react-router-dom";
 import AppRouter from "./components/AppRouter";
+import Footer from "./components/UI/footer/footer";
+import React from "react";
 
 
 function App() {
   return (
-    <div className="App">
-        <BrowserRouter>
-            <NavBar/>
-            <AppRouter/>
-        </BrowserRouter>
+    <div className="AppContent">
+        <div className='appContainer'>
+            <BrowserRouter>
+                <NavBar/>
+                <AppRouter/>
+            </BrowserRouter>
+        </div>
+            <hr style={{marginLeft: '80px', marginRight: '80px'}}/>
+            <Footer/>
     </div>
   );
 }
 
 export default App;
+
