dd887f60ed492872637fe5f3ef58e4ccedf9cf5a
Alex <panches2001@gmail.com>
2022-11-09 01:46:51 +0300

    grid and anims

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -9,14 +9,36 @@ import {useFetching} from "../components/hooks/useFetching";
 import Loader from "../components/UI/loader/loader";
 import PostService from "../components/API/PostService";
 import styled, {keyframes} from "styled-components";
-import {shake} from "react-animations";
-import goblin from '../pictures/goblin.png'
+import {shake, fadeOutLeft, fadeOutRight, pulse} from "react-animations";
+import scheme from '../pictures/scheme.jpeg'
 
-const bounceAnimation = keyframes`${shake}`;
+const shakeAnimation = keyframes`${shake}`;
+const outLeftAnimation = keyframes `${fadeOutLeft}`
+const inLeftAnimation = keyframes `${fadeOutRight}`
+const pulseAnimation = keyframes`${pulse}`
 
-const BouncyDiv = styled.div`
+const PulseDiv = styled.div`
   &:hover{
-    animation: 1s ${bounceAnimation};
+    animation: 1s ${pulseAnimation};
+  }
+`;
+
+const ShakeDiv = styled.div`
+  &:hover{
+    animation: 1s ${shakeAnimation};
+  }
+`;
+
+const OutDiv = styled.div`
+  &:hover{
+    animation: 0.5s ${outLeftAnimation};
+  }
+`;
+
+const InDiv = styled.div`
+  &:hover{
+    animation: 0.5s ${inLeftAnimation};
+    animation-iteration-count: 1;
   }
 `;
 
@@ -50,29 +72,59 @@ const Home = () => {
             <h1 style={{'font-family': 'Roboto Mono'}}>Kaiiisaka's page</h1>
             <Container id='containerhome'>
                 <p>
-                    Hi! I'm kaiiisaka (real name - Aleksandr). I am a 4th year student at BSTU "VOENMEH" in St. Petersburg and I am a junior front-end developer.
+                    Hi! I'm kaiiisaka (real name - Aleksandr). I am a 4th year student at BSTU "VOENMEH"
+                    in St. Petersburg and I am a junior front-end developer.
                 </p>
                 <p>
-                    This site is my portfolio. Studying it, you will understand what I can do here and now. But do not think that I am not learning anything. Even if there is no functionality here, then I am only learning to implement it.
+                    This site is my portfolio. Studying it, you will understand what I can do here and now.
+                    But do not think that I am not learning anything. Even if there is no functionality here,
+                    then I am only learning to implement it.
                 </p>
                 <Accordion1 />
             </Container>
             <Slider/>
             <Container className="content-home">
             <Jumbotron />
-                {isUserLoading
-                    ? <Loader/>
-                    : <UserList users={users} title='Online users' />
-                }
-                <BouncyDiv>
-                    <div className='goblin'>
-                        <h2>Ебучая анимация весь день просрал</h2>
-                        <img
-                        src={goblin}
-                        height='300px'
-                        />
+                <h2 style={{margin: '1rem', border: '2px solid black', borderRadius: '10px', padding: '10px'}}>Okay, crypto, what else?</h2>
+                <div className='apiWrapper'>
+                    <div className='users'>
+                        {isUserLoading
+                            ? <Loader/>
+                            : <UserList users={users} title='Online users' />
+                        }
                     </div>
-                </BouncyDiv>
+                    <div className='tiles'>
+                        <div className='tile1'>
+                        <ShakeDiv>
+                                <h2>Fake API</h2>
+                                <p>Is it real users? No! It's just API demo.</p>
+                                <p>Let's see how it works.</p>
+                        </ShakeDiv>
+                        </div>
+                        <div className='tile2'>
+                            <OutDiv>
+                            <h2>Request</h2>
+                            <p>My site is requesting data that is on a site jsonplaceholder.com.
+                                There they are stored in a special format.
+                            </p>
+                            </OutDiv>
+                        </div>
+                        <div className='tile3'>
+                            <InDiv>
+                            <h2>Answer</h2>
+                            <p>
+                                When a site receives a request, it sends data to that site using the http protocol.
+                            </p>
+                            </InDiv>
+                        </div>
+                        <div className='tile4'>
+                            <PulseDiv>
+                            <h2>The picture is clearer!</h2>
+                            <img src={scheme} width='100%'/>
+                            </PulseDiv>
+                        </div>
+                    </div>
+                </div>
             </Container>
         </div>
     );
